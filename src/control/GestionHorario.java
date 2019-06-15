7bd220277f6de210761f8511d654d2eeb36c2c91
RicardoMilos <49229984+ElParoEsMalo@users.noreply.github.com>
2019-06-15 22:42:39 +0200

    Update GestionHorario.java

--- src/control/GestionHorario.java
+++ src/control/GestionHorario.java
@@ -14,6 +14,8 @@ public class GestionHorario {
 			DiasDeLaSemana.ThursDay, DiasDeLaSemana.Friday };
 	DiasDeLaSemana[] diaEspecilistaPar = new DiasDeLaSemana[2];
 	DiasDeLaSemana[] diaEspecilistaImpar = new DiasDeLaSemana[3];
+	/*Este vector te indica cuantas consultas hay en la posicion 0 se guarda la mañana y la 1 se 
+	 * guarda las consultas de la tarde*/
 	int[] consultasPrimaria = new int[2];
 	int[] consultasEpecialistas = new int[2];
 	Horario[] horariosPrimaria = new Horario[2];
@@ -32,7 +34,8 @@ public class GestionHorario {
 		horariosEspecialista[1] = new Horario(horas, diaEspecilistaImpar);
 
 	}
-
+	/**Esta funcion crea los horarios de Especialista
+	 * @return*/
 	private void asignarDias() {
 		int indicePar=0;
 		int indice=0;
@@ -54,7 +57,7 @@ public class GestionHorario {
 	public int[] getConsultasEpecialistas() {
 		return consultasEpecialistas;
 	}
-
+	
 	public void ocuparConsultaEspecialista(int turno, Medico medico) {
 		consultasEpecialistas[turno] += 1;
 		medico.setHorario(horariosPrimaria[turno].getHoraTrabajo());
@@ -70,7 +73,9 @@ public class GestionHorario {
 	public boolean turnoDisponible(int turno) {
 		return consultasPrimaria[turno] < 4;
 	}
-
+/**Crea el horario de Los medicos de atencion primaria
+ @return
+ */
 	private void iniciarHorarioPrimaria() {
 		int ultimaHora = 8;
 		for (int i = 0; i < horariosPrimaria.length; i++) {
